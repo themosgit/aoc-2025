7e2fda0e70d6ce27cbf9dc14977054c75f321e8a
Themos Papatheofanous <themos360@gmail.com>
2025-12-04 01:38:38 +0200

    improving readability

--- days/day03/day03.ml
+++ days/day03/day03.ml
@@ -3,4 +3,4 @@ let run part =
     | Some 1 -> Part1.solve ()
     | Some 2 -> Part2.solve ()
     | Some n -> Printf.eprintf "Error: %d is an invalid part number\n" n
-    | None -> Part1.solve ()
+    | None -> Part1.solve () ; Part2.solve ()
--- days/day03/part2.ml
+++ days/day03/part2.ml
@@ -1,52 +1,61 @@
 let file = "days/day03/input.txt"
 
+(* change to 2 for part 1 *)
+let jolt_size = 12 
+
+(* retrieves new lines *)
 let return_line input =
     Seq.of_dispenser (fun () -> In_channel.input_line input)
 
-let compute_start i line =
-     let index = 12 - (String.length line - i) in
+(** 
+ * calculate earliest array idx that may be modified
+ * if we have 10 chars left and jolt_size is 12 this
+ * will return 2 meaning we can start modifying from
+ * the second element in the array
+ **)
+let array_idx i line =
+     let index = jolt_size - (String.length line - i) in
      if index < 0 then 0 else index
 
-let rec set_joltarray j joltarray digit =
-    if j >= 12 then joltarray
+(**
+ * checks if digit is max of some array member if
+ * so resets all following members to -1 
+ **)
+let rec set_array idx joltarray digit =
+    if idx >= jolt_size then joltarray
     else
-        if joltarray.(j) < digit then begin
-            joltarray.(j) <- digit;
-            set_joltarray (j + 1) joltarray (-1)
+        if joltarray.(idx) < digit then begin
+            joltarray.(idx) <- digit;
+            set_array (idx + 1) joltarray (-1)
         end
-        else if digit == -1 then begin
-            joltarray.(j) <- digit;
-            set_joltarray (j + 1) joltarray digit
+        else if digit = -1 then begin
+            joltarray.(idx) <- -1;
+            set_array (idx + 1) joltarray (-1) 
         end
         else
-        set_joltarray (j + 1) joltarray digit
+        set_array (idx + 1) joltarray digit
 
-
-
-let sum_jolts line =
-    let joltarray = Array.make 12 (-1) in
-    let rec loop i =
-        if i >= String.length line then ()
+(* populates array with max digits and returns the sum *)
+let compute_line line =
+    let array = Array.make jolt_size (-1) in
+    let rec populate_array i array =
+        if i >= String.length line then array 
         else
             let digit = Char.code line.[i] - Char.code '0' in
-            let _  = set_joltarray (compute_start i line) joltarray digit in
-            loop (i + 1)
+            populate_array (i + 1) (set_array (array_idx i line) array digit)
     in
-    loop 0;
-    let rec compute_sum i multiplier acc =
-        if i < 0 then acc
-        else compute_sum (i - 1) (multiplier * 10) 
-            (acc + joltarray.(i) * multiplier)
+    let joltarray = populate_array 0 array in
+    let rec sum_elements i mult accu =
+        if i < 0 then accu
+        else sum_elements (i - 1) (mult * 10) (accu + joltarray.(i) * mult)
     in
-    compute_sum 11 1 0
+    sum_elements (jolt_size - 1) 1 0
     
-
-
 let solve () =
     In_channel.with_open_text file @@ fun input ->
     let sum = 
         return_line input
-        |> Seq.map sum_jolts 
+        |> Seq.map compute_line 
         |> Seq.fold_left (+) 0
     in
     Printf.printf "Sum of jolts: %d\n" sum
